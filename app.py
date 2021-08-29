531ed1905484ad50d90b5d2779ff09c91509302a
Yeabsira <yeabsiram@outlook.com>
2021-08-29 21:25:18 +0300

    setting up app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
- 
+from flask import Flask, jsonify, request, session
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
+from settings import *
+from model import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+api = Api(app)
+db.init_app(app)
+CORS(app)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
