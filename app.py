2b224efe1b9407c80c0a0778b4262da05c567a97
Natnael123456 <it.natnael.zerihun@gmail.com>
2021-08-30 20:46:07 -0700

    user post endpoint done

--- app.py
+++ app.py
@@ -27,6 +27,71 @@ class User(Resource):
             return userInfo.serialize(), 200
 
         return 'no user found', 404
+    def post(self):
+
+        new_user_info = json.loads(request.form['content'])
+
+        username = new_user_info['username']
+        email = new_user_info['email']
+        password = new_user_info['password']
+        sex = new_user_info['sex']
+        age = new_user_info['age']
+        question = new_user_info['question']
+        answer = new_user_info['answer']
+        height = new_user_info['height']
+        weight = new_user_info['weight']
+
+        if checkLen(username, 4):
+
+            if isUsernameUnique(username):
+
+                if valEmail(email):
+
+                    if isEmailUnique(email):
+
+                        if checkLen(password, 8):
+
+                            if isBetween(age, 15, 75):
+
+                                if isBetween(height, 1.45, 2.5):
+
+                                    if isBetween(weight, 25, 200):
+
+                                        new_user = Users(
+                                            username, email, password, sex, age, height, weight, question, answer)
+                                        new_user_credentials = Login(
+                                            email, password, "user")
+
+                                        db.session.add(new_user)
+                                        db.session.commit()
+
+                                        db.session.add(new_user_credentials)
+                                        db.session.commit()
+
+                                        return "Success", 200
+
+                                    else:
+                                        return "invalid weight", 401
+
+                                else:
+                                    return "invalid height", 401
+
+                            else:
+                                return "invalid age", 401
+
+                        else:
+                            return "short password", 401
+
+                    else:
+                        return "email taken", 401
+
+                else:
+                    return "invalid email", 401
+
+            else:
+                return "username taken", 401
+
+        return "short username", 401    
  api.add_resource(User, '/api/User/')
 
 if __name__ == "__main__":
