6132e3880567590aaa7e6011c64e9c109ae9e7d2
Natnael123456 <it.natnael.zerihun@gmail.com>
2021-08-30 18:34:12 -0700

    user get endpoint done

--- app.py
+++ app.py
@@ -12,6 +12,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 api = Api(app)
 db.init_app(app)
 CORS(app)
+class User(Resource):
+
+    def get(self):
+
+        # update_diet_header = request.headers
+        user_id = 1 
+
+        userInfo = Users.query.filter(
+            Users.user_id == user_id).first()
+
+        if userInfo:
+
+            return userInfo.serialize(), 200
+
+        return 'no user found', 404
+ api.add_resource(User, '/api/User/')
 
 if __name__ == "__main__":
     app.run(debug=True) 
