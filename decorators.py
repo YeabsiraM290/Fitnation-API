82f071cb7305fcdf32a918e99d3cff64cadb3239
Yeabsira <yeabsiram@outlook.com>
2021-09-03 19:42:16 +0300

    token decorator

---
+++ decorators.py
@@ -0,0 +1,30 @@
+from flask import request
+from model import *
+from functools import wraps
+import jwt
+from settings import SECERET_KEY
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'token' in request.headers:
+            token = request.headers['token']
+
+        if not token:
+            return "Token is required!", 401
+
+        try:
+            data = jwt.decode(token, SECERET_KEY, algorithms=["HS256"])
+            current_user = Users.query.filter(Users.email == data['email']).first()
+            is_user_allowed = LoginInfo.query.filter(LoginInfo.email == current_user.email).first()
+            if is_user_allowed == None:
+                return "Account is blocked!", 400
+        except:
+           
+            return "Token is invalid!", 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated   
